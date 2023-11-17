5264db2f1bdd7341ec291696de9815c24b857e35
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2023-11-17 15:52:58 +0100

    Article deletion

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -2,3 +2,8 @@
 <article>
     {{ $article->body }}
 </article>
+<form method="POST" action="{{ route("articles.destroy",$article) }}">
+    @csrf
+    @method("DELETE")
+    <input type="submit" value="Supprimer">
+</form>
