59808af8f3c4234892d98291922b2206bc7ef324
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2023-12-01 15:43:27 +0100

    refactor : using blade instead of php syntax

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -12,16 +12,16 @@
 </form>
 <a href="{{route("articles.edit",$article)}}">Modifier</a>
 <h3>Comments :</h3>
-<?php foreach ($comments as $comment): ?>
-<p><?=$comment->body?></p>
-<?php endforeach; ?>
-<?php if($article->archived_at == null): ?>
+@foreach($comments as $comment)
+<p>{{ $comment->body  }}</p>
+@endforeach
+@if($article->archived_at == null)
 <h3>Post a new comment</h3>
 <form method="POST" action="{{ route("articles.comments.store",["article"=>$article]) }}">
     @csrf
     @include('comments.fields')
 </form>
-<?php else: ?>
+@else
 <h3>You cannot post a comment on an archived article!</h3>
-<?php endif;?>
+@endif
 @endsection
