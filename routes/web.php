70cd06257e9acb60def9e9347b041ae34439d0a9
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2023-12-01 15:47:19 +0100

    refactor : using controllers for more cleaner code

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\CommentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('articles',\App\Http\Controllers\ArticleController::class);
-Route::resource('articles.comments',\App\Http\Controllers\CommentController::class)->only(["store"]);
+Route::resource('articles',ArticleController::class);
+Route::resource('articles.comments',CommentController::class)->only(["store"]);
