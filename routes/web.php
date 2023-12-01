2367346d994fb6e8fe1a4c93928aea1c2eca0252
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2023-12-01 15:44:56 +0100

    refactor : using only store route for comment controller

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('articles.comments',\App\Http\Controllers\CommentController::class);
 Route::resource('articles',\App\Http\Controllers\ArticleController::class);
+Route::resource('articles.comments',\App\Http\Controllers\CommentController::class)->only(["store"]);
