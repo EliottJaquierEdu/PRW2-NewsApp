edc80983db76e38574c8dcbcd6d36ee032e51f46
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2023-12-01 15:46:13 +0100

    refactor : make article_id fk cascading on delete

--- database/migrations/2023_12_01_125827_comment-link-with-article.php
+++ database/migrations/2023_12_01_125827_comment-link-with-article.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table("comments", function (Blueprint $table) {
-            $table->foreignId("article_id");
+            $table->foreignId("article_id")->constrained()->cascadeOnDelete();
         });
     }
 
